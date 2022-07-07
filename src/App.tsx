0779dc79b05c688943bd600bbd1ed175a20a84b9
JulioNunesDev <julionunes999@gmail.com>
2022-07-07 05:16:18 -0300

    first commit project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ButtoGreen from "./shared/components/ButtonGreen/ButtoGreen"
+import {GlobalStyles} from './styles/GlobalStyles'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <ButtoGreen  />
+     <GlobalStyles/>
+    </div>
+  )
+}
+
+export default App
