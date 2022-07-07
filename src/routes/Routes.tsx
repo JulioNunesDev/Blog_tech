9cbb3747c851c9b4da066d4d9616fa15c749020a
JulioNunesDev <julionunes999@gmail.com>
2022-07-07 08:50:48 -0300

    feat: routes

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+
+const RoutesGet = () =>{
+    return (
+        <Routes>
+            <Route  path="/" element={''}/>
+            <Route  path="/categorias" element={''}/>
+            <Route  path="/blog" element={''}/>
+            <Route  path="/lifestyles" element={''}/>
+            <Route  path="/contact" element={''}/>
+            <Route  path="/blog" element={''}/>
+            <Route  path="/404" element={''}/>
+
+        </Routes>
+    )
+}
+
+export default RoutesGet
