682c86b63fc93b8ec6d90ee4d13c672df5511108
JulioNunesDev <julionunes999@gmail.com>
2022-07-08 21:17:48 -0300

    feat: data update moment

--- src/layouts/partials/H_posts_recents/H_posts_recents.tsx
+++ src/layouts/partials/H_posts_recents/H_posts_recents.tsx
@@ -1,12 +1,14 @@
 import { Base_Posts_Recents, Content_Posts_Recents } from "./StyH_posts_recents"
 import { useEffect, useState } from "react"
 import api from "../../../services/Api"
-import Moment from "react-moment"
 import moment from "moment"
+import 'moment/locale/pt-br'
 
 
 const H_Posts_Recents = () =>{
-    const [dados, setDados] = useState<any | null>(null)
+    const [dados, setDados] = useState<any>(null)
+    const [dataPublicao, setDataPublic] = useState<any>([])
+    const [capa, setCapa] = useState<any>(0)
 
 
     useEffect(()=>{
@@ -22,15 +24,27 @@ const H_Posts_Recents = () =>{
                 console.log('Erro na requisicao: ', err);
             })
         }
+        const horas = dados?.[0].publishedAt
+        const tempo = moment(horas).startOf('hours').fromNow()
         ReqRes()
-        console.log(dados?.[0].publishedAt);
+        setDataPublic(tempo)
+      
+        setInterval(()=>{
+            let random = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+            let valor = random[Math.floor(Math.random()*random.length)]
+            setCapa(valor)
+        }, 1000 * 100)
+        console.log(capa);
+
     },[])
 
+    
+   
 
 
 
     return(
-        <Base_Posts_Recents>
+        <Base_Posts_Recents postCapa={dados?.[capa].urlToImage}>
             <Content_Posts_Recents test={dados?.[0].urlToImage}  posts={dados?.[1].urlToImage} postsTwo={dados?.[4].urlToImage} >
                     <a href={dados?.[0].url} target="_blank">
             <div className="post_recents_main" >
@@ -40,7 +54,7 @@ const H_Posts_Recents = () =>{
                         <img src={dados?.[0].urlToImage} alt="" />
                         <p>{dados?.[0].author} • </p>
                         
-                        <span className="post_infors_date"><p>{moment(dados?.[0].publishedAt).subtract(0, 'days').calendar()}</p></span>
+                        <span className="post_infors_date"><p>{dataPublicao}</p></span>
                         
                     </span>
                     </div>
@@ -54,7 +68,7 @@ const H_Posts_Recents = () =>{
                     <span className="post_infors">
                         <img src={dados?.[1].urlToImage} alt="" />
                         <p>{dados?.[1].author} • </p>
-                        <span className="post_infors_date"><p>{moment(dados?.[1].publishedAt).subtract(0, 'days').calendar()}</p></span>
+                        <span className="post_infors_date"><p>{dataPublicao}</p></span>
                     </span>
                     </div>
             </div>
@@ -65,8 +79,8 @@ const H_Posts_Recents = () =>{
                     <h1>{dados?.[4].title.substring(0, 75)+'...'}</h1>
                     <span className="post_infors">
                         <img src={dados?.[2].urlToImage} />
-                        <p>{dados?.[2].author?.substring()+''} • </p>
-                        <span className="post_infors_date"><p>{moment(dados?.[2].publishedAt).subtract(0, 'days').calendar()}</p></span>
+                        <p>{dados?.[2].author} • </p>
+                        <span className="post_infors_date"><p>{dataPublicao}</p></span>
                     </span>
                     </div>
             </div>
