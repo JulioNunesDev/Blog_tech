53076033df40dfb81ec9169a058a951ce40c4d5d
JulioNunesDev <julionunes999@gmail.com>
2022-07-08 21:18:34 -0300

    update: moment

--- src/layouts/partials/H_posts_recents/H_posts_recents.tsx
+++ src/layouts/partials/H_posts_recents/H_posts_recents.tsx
@@ -33,7 +33,7 @@ const H_Posts_Recents = () =>{
             let random = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
             let valor = random[Math.floor(Math.random()*random.length)]
             setCapa(valor)
-        }, 1000 * 100)
+        }, 10000)
         console.log(capa);
 
     },[])
